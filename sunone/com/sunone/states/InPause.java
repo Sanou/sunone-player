cf72f7af59bb4a03b8b418b725e6d62fd78465ae
msanoud <msanoud@77ba566a-b93c-0410-aea7-5b182a79bc97>
2008-08-06 09:46:49 +0000

    Professional version.

---
+++ sunone/com/sunone/states/InPause.java
@@ -0,0 +1,26 @@
+package com.sunone.states;
+
+import com.sunone.SunoneException;
+import com.sunone.SunoneState;
+import com.sunone.Lecteur.SunoneStates;
+
+public class InPause extends SunoneState{
+
+
+	public SunoneStates stop() throws SunoneException {
+		// TODO Auto-generated method stub
+		return SunoneStates.IN_STOP;
+	}
+	
+	
+	public SunoneStates play() throws SunoneException {
+		// TODO Auto-generated method stub
+		return SunoneStates.IN_PLAY;
+	}
+	
+	
+	public SunoneStates pause() throws SunoneException {
+		// TODO Auto-generated method stub
+		return SunoneStates.IN_PLAY;
+	}
+}
