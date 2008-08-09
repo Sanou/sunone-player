cab36059f5d9ef3f74945a4b56de0ba0e13f9834
msanoud <msanoud@77ba566a-b93c-0410-aea7-5b182a79bc97>
2008-08-09 14:13:14 +0000

    Last.

--- sunone/resources/playlist/current_playlist.pl
+++ sunone/resources/playlist/current_playlist.pl
@@ -1,9 +1,1 @@
-D:\Divertissement et utilitaires\Discours historique\fdrarsenalofdemocracy7.mp3
-D:\Divertissement et utilitaires\Discours historique\fdrwarmessage344.mp3
-D:\Divertissement et utilitaires\Discours historique\geraldfordpardonixon434348.mp3
-D:\Divertissement et utilitaires\Discours historique\jfkinaugural1sfdsfdsfdsfszz.mp3
-D:\Divertissement et utilitaires\Discours historique\mlking1.wav
-D:\Divertissement et utilitaires\Discours historique\mlkingihaveadreamtu6trtrtr766.mp3
-D:\Divertissement et utilitaires\Discours historique\roosevelt.mp3
-D:\Divertissement et utilitaires\Discours historique\trumann.wav
 D:\Divertissement et utilitaires\Discours historique\Bill Clinton\billclintonoklahomaprayerservice34583458439.mp3
