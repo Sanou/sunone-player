3714ded2369e8ebe658b43e62e447f1fdc6594d1
msanoud <msanoud@77ba566a-b93c-0410-aea7-5b182a79bc97>
2009-02-23 10:03:52 +0000

    arrangement des logs.

--- sunone/com/sunone/FileEdited.java
+++ sunone/com/sunone/FileEdited.java
@@ -1,26 +1,24 @@
 package com.sunone;
 
-//import java.io.*;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import org.apache.commons.configuration.PropertiesConfiguration;
 import org.apache.log4j.Logger;
 
-//import java.net.URL;
 public class FileEdited
 {
   static Logger logger = Logger.getLogger(FileEdited.class);
 
-  public static BufferedReader openFile(String filename, int positionline)
+  public static BufferedReader openFile(String pFilename, int pPositionLine)
       throws Exception
   {
-    BufferedReader br = new BufferedReader(new FileReader(new File(filename)));
+    BufferedReader br = new BufferedReader(new FileReader(new File(pFilename)));
     int i = 0;
-    while (i < positionline)
+    while (i < pPositionLine)
     {
-      i++;
       br.readLine();
+      i++;
     }
     return br;
   }
@@ -28,29 +26,27 @@ public class FileEdited
   public static void saveSunoneParameters(PropertiesConfiguration configuration)
       throws Exception
   {
-    // PrintWriter pw=new PrintWriter(new BufferedWriter(new FileWriter(new
-    // File(filename))));
     configuration.setAutoSave(true);
     configuration.save();
-    if (logger.isDebugEnabled())
-    {
-      logger.debug("Saving Sunone Parameters");
-      logger.debug("-->LOOK AND FEEL: " + GUI.lf);
-    }
     configuration.setProperty("com.sunone.currentlookandfeel", new Integer(GUI.lf));
-    logger.debug("-->CURRENT PLAYLIST NAME: " + Playlist.currentNAME);
     configuration.setProperty("com.sunone.playlist.currentname", Playlist.currentNAME);
-    logger.debug("-->CURRENT INDEX MEDIA: " + Lecteur.CURRENTINDEXMEDIA);
     configuration.setProperty("com.sunone.playlist.currentindexofmedia", new Integer(Lecteur.CURRENTINDEXMEDIA));
-    logger.debug("-->REPEAT PLAYLIST ACTIVATION: " + Lecteur.REPEATPLAYLIST);
     configuration.setProperty("com.sunone.playlist.repeatplaylist.activation", new Integer(Lecteur.REPEATPLAYLIST));
-    logger.debug("-->REPEAT MEDIA ACTIVATION: " + Lecteur.REPEATMEDIA);
     configuration.setProperty("com.sunone.playlist.repeatmedia.activation", new Integer(Lecteur.REPEATMEDIA));
 
     if (GUI.currentDirectory != null)
     {
-      logger.debug("-->CURRENT FILE CHOOSER DIRECTORY: " + GUI.currentDirectory);
       configuration.setProperty("com.sunone.currentdirectory", GUI.currentDirectory.toString());
+    }
+    if (logger.isDebugEnabled())
+    {
+      logger.debug("-->REPEAT MEDIA ACTIVATION: " + Lecteur.REPEATMEDIA);
+      logger.debug("-->CURRENT PLAYLIST NAME: " + Playlist.currentNAME);
+      logger.debug("-->REPEAT PLAYLIST ACTIVATION: " + Lecteur.REPEATPLAYLIST);
+      logger.debug("-->CURRENT INDEX MEDIA: " + Lecteur.CURRENTINDEXMEDIA);
+      logger.debug("Saving Sunone Parameters");
+      logger.debug("-->LOOK AND FEEL: " + GUI.lf);
+      logger.debug("-->CURRENT FILE CHOOSER DIRECTORY: " + GUI.currentDirectory);
     }
     configuration.save();
   }
@@ -58,23 +54,24 @@ public class FileEdited
   public static void restoreSunoneParameters(PropertiesConfiguration configuration)
       throws Exception
   {
-
-    if (logger.isDebugEnabled())
-      logger.debug("Restoring Sunone Parameters");
     GUI.lf = configuration.getInt("com.sunone.currentlookandfeel");
-    logger.debug("-->LOOK AND FEEL: " + GUI.lf);
     Playlist.currentNAME = configuration.getString("com.sunone.playlist.currentname");
-    logger.debug("-->CURRENT PLAYLIST NAME: " + Playlist.currentNAME);
     Lecteur.CURRENTINDEXMEDIA = configuration.getInt("com.sunone.playlist.currentindexofmedia");
-    logger.debug("-->CURRENT INDEX MEDIA: " + Lecteur.CURRENTINDEXMEDIA);
     Lecteur.REPEATPLAYLIST = configuration.getInt("com.sunone.playlist.repeatplaylist.activation");
-    logger.debug("-->REPEAT PLAYLIST ACTIVATION: " + Lecteur.REPEATPLAYLIST);
     Lecteur.REPEATMEDIA = configuration.getInt("com.sunone.playlist.repeatmedia.activation");
-    logger.debug("-->REPEAT MEDIA ACTIVATION: " + Lecteur.REPEATMEDIA);
     String Maimouna = configuration.getString("com.sunone.currentdirectory");
     if (Maimouna != null && !Maimouna.equals(""))
       GUI.currentDirectory = new File(Maimouna);
-    logger.debug("-->CURRENT FILE CHOOSER DIRECTORY: " + GUI.currentDirectory);
+    if (logger.isDebugEnabled())
+    {
+      logger.debug("Restoring Sunone Parameters");
+      logger.debug("-->LOOK AND FEEL: " + GUI.lf);
+      logger.debug("-->CURRENT PLAYLIST NAME: " + Playlist.currentNAME);
+      logger.debug("-->CURRENT INDEX MEDIA: " + Lecteur.CURRENTINDEXMEDIA);
+      logger.debug("-->REPEAT PLAYLIST ACTIVATION: " + Lecteur.REPEATPLAYLIST);
+      logger.debug("-->REPEAT MEDIA ACTIVATION: " + Lecteur.REPEATMEDIA);
+      logger.debug("-->CURRENT FILE CHOOSER DIRECTORY: " + GUI.currentDirectory);
+    }
   }
 
 }
