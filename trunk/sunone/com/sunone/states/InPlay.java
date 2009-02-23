893d5b0a5a955160830cc0ae46ba58ecb9d2ca04
msanoud <msanoud@77ba566a-b93c-0410-aea7-5b182a79bc97>
2009-02-23 09:40:02 +0000

    formattage du code.

--- trunk/sunone/com/sunone/states/InPlay.java
+++ trunk/sunone/com/sunone/states/InPlay.java
@@ -4,29 +4,32 @@ import com.sunone.SunoneException;
 import com.sunone.SunoneState;
 import com.sunone.Lecteur.SunoneStates;
 
-public class InPlay extends SunoneState{
-	@Override
-	public SunoneStates pause() throws SunoneException {
-		// TODO Auto-generated method stub
-		return SunoneStates.IN_PLAY;
-	}
-	
-	@Override
-	public SunoneStates stop() throws SunoneException {
-		// TODO Auto-generated method stub
-		return SunoneStates.IN_STOP;
-	}
-	
-	
-	public SunoneStates next() throws SunoneException {
-		// TODO Auto-generated method stub
-		return SunoneStates.IN_PLAY;
-	}
-	
-	
-	public SunoneStates previous() throws SunoneException {
-		// TODO Auto-generated method stub
-		return SunoneStates.IN_PLAY;
-	}
+public class InPlay extends SunoneState
+{
+  @Override
+  public SunoneStates pause() throws SunoneException
+  {
+    // TODO Auto-generated method stub
+    return SunoneStates.IN_PLAY;
+  }
+
+  @Override
+  public SunoneStates stop() throws SunoneException
+  {
+    // TODO Auto-generated method stub
+    return SunoneStates.IN_STOP;
+  }
+
+  public SunoneStates next() throws SunoneException
+  {
+    // TODO Auto-generated method stub
+    return SunoneStates.IN_PLAY;
+  }
+
+  public SunoneStates previous() throws SunoneException
+  {
+    // TODO Auto-generated method stub
+    return SunoneStates.IN_PLAY;
+  }
 
 }
